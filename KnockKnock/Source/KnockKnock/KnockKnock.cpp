3c6d85df33b736267cb28d50383c533471fe5793
Minii23 <Milly.Walker3581@gmail.com>
2026-01-28 13:43:48 +1100

    Update 0.1

---
+++ KnockKnock/Source/KnockKnock/KnockKnock.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "KnockKnock.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KnockKnock, "KnockKnock" );
